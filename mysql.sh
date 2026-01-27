f4497c38c89528d8ed23ab1c6f4e681c1833a4e5
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2026-01-27 20:53:26 +0530

    expense project automation

---
+++ mysql.sh
@@ -0,0 +1,42 @@
+#!bin/bash
+
+USER_ID=$(id -u)
+
+CHECK_ROOT(){
+    if [ USER_ID -ne 0 ]
+    then
+        echo " You must need sudo permissions "
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo " $2....:: FAILURE"
+    else
+        echo "$2.....:: SUCCESS "
+    fi
+}
+
+CHECK_ROOT
+
+
+dnf install mysql-server -y
+VALIDATE $? "MYSQL server installing"
+
+systemctl enable mysqld
+VALIDATE $? "ENABLING Mysql"
+
+systemctl start mysqld
+VALIDATE $? "Starting MYSQL"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1 | show databases;
+if [ $? -ne 0 ]
+then
+    echo "ROOT PASSWORD NOT set"
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+fi
+
+
+
