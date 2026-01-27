b66decd9ba7cb033374a3f9d39b6e6a05f01f25a
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2026-01-27 22:41:39 +0530

    expense project automation

--- backend.sh
+++ backend.sh
@@ -59,7 +59,7 @@ mkdir -p etc/systemd/system/backend.service
 VALIDATE $? "Creating backend service"
 
 cp "/home/ec2-user/Expense-shell/backend.service" "etc/systemd/system/backend.service"
-
+VALIDATE $? "COPY backend service"
 
 dnf install mysql -y
 VALIDATE $? "Install MYSQL"
