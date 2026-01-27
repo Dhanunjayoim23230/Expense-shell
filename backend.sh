dc1d93044260ed4b26410b7389048c330f371979
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2026-01-27 22:20:12 +0530

    expense project automation

--- backend.sh
+++ backend.sh
@@ -42,6 +42,7 @@ fi
 mkdir -p /app
 VALIDATE $? "Creating app directory"
 
+
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
 VALIDATE $? "Downloading code"
 
